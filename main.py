8b93dafacf181efa4eb401bb8220707a07d0090f
Domenico Gaudioso <43296235+DomenicoGaudioso@users.noreply.github.com>
2024-06-14 00:26:15 +0200

    Update main.py

--- main.py
+++ main.py
@@ -67,8 +67,7 @@ def createExcelfromMultipleDataFrame(listDataFrame, listName, path):
 
 def openFolder(pathFolder, type=None):
     # Ottieni la lista di elementi nella cartella (sia cartelle che file)
-    pathFolder= os.path.encode(pathFolder).decode()
-    elenco_elementi = os.listdir(pathFolder)
+    elenco_elementi = os.listdir(r'%s' %pathFolder)
     insedeFolder = []
     insedeFile = []
     # Stampa sia cartelle che file in base al parametro 'type'
